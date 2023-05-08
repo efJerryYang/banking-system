d14cb487955a9acf70196cb5c46eb80516b25dad
Jerry Yang <efjerryyang@outlook.com>
2023-05-08 20:40:11 +0800

    feat(view): add views for login and logout pages

--- banking-system-frontend/src/components/LoginPage.vue
+++ banking-system-frontend/src/components/LoginPage.vue
@@ -1,14 +1,95 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Login Page</h1>
-    <!-- TODO -->
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" class="form-control" id="username" v-model="username" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" v-model="password" />
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
   </div>
 </template>
 
 <script setup>
-// TODO
+import { ref } from 'vue';
+
+const username = ref('');
+const password = ref('');
+
+const submitForm = () => {
+  // TODO
+  // Submit form to server
+  console.log('Username:', username.value);
+  console.log('Password:', password.value);
+};
 </script>
 
 <style scoped>
-/* TODO */
+.container {
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.form-control {
+  display: block;
+  width: 100%;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.btn {
+  display: inline-block;
+  margin-bottom: 0;
+  font-weight: normal;
+  text-align: center;
+  vertical-align: middle;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  border-radius: 4px;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #337ab7;
+  border-color: #2e6da4;
+}
+
+.btn-primary:hover {
+  background-color: #286090;
+  border-color: #204d74;
+}
 </style>
