18394388598e8d4fed909e7e27108404e9994ef6
Jerry Yang <efjerryyang@outlook.com>
2023-05-08 11:39:29 +0800

    feat(router): add routes for necessary components

--- banking-system-frontend/src/router/index.js
+++ banking-system-frontend/src/router/index.js
@@ -1,23 +1,77 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '@/components/LoginPage.vue'
+import LogoutPage from '@/components/LogoutPage.vue'
+import TheDashboard from '@/components/TheDashboard.vue'
+import CreateAccount from '@/components/CreateAccount.vue'
+import DeleteAccount from '@/components/DeleteAccount.vue'
+import AccountBalance from '@/components/AccountBalance.vue'
+import TransferFunds from '@/components/TransferFunds.vue'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    component: LogoutPage
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: TheDashboard
+  },
+  {
+    path: '/create-account',
+    name: 'create-account',
+    component: CreateAccount
+  },
+  {
+    path: '/delete-account',
+    name: 'delete-account',
+    component: DeleteAccount
+  },
+  {
+    path: '/account-balance',
+    name: 'account-balance',
+    component: AccountBalance
+  },
+  {
+    path: '/transfer-funds',
+    name: 'transfer-funds',
+    component: TransferFunds
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
+
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/AboutView.vue')
+//     }
+//   ]
+// })
+
+// export default router
