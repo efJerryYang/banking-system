0f066563344ac94f79f09000142626128d5042fb
Jerry Yang <efjerryyang@outlook.com>
2023-05-08 12:10:02 +0800

    feat(store): add global status management

--- banking-system-frontend/src/main.js
+++ banking-system-frontend/src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import axios from 'axios'
 
@@ -11,5 +12,6 @@ axios.defaults.baseURL = '/'
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
